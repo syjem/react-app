643c641dc38cf38fa2dcc8d47bd00009e8dbabb6
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-03-09 16:46:05 +0800

    my first react

--- src/App.js
+++ src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello JSX!</h1>
+      <h2>This is my First React</h2>
+      <p>Thank you!</p>
     </div>
   );
 }
