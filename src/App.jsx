71b8d80195c2733c7ba669f435ad9af796536e8c
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-03-09 21:19:40 +0800

    first react app

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,12 @@ function App() {
     <div className="App">
       <h1>Hello JSX!</h1>
       <h2>This is my First React</h2>
-      <p>Thank you!</p>
+      <p>My Libraries and Tools</p>
+      <ul>
+        <li>React</li>
+        <li>SASS</li>
+        <li>jQuery</li>
+      </ul>
     </div>
   );
 }
